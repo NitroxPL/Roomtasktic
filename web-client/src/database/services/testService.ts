ebafc6eb8876657b691fc254cd2c860af967d627
NitroxPL <pawelpardel2@wp.pl>
2023-06-13 02:48:42 +0200

    Typo fix | added try-catch block

--- web-client/src/database/services/testService.ts
+++ web-client/src/database/services/testService.ts
@@ -1,11 +1,16 @@
 import { doc, setDoc } from "firebase/firestore";
 import { testCollection } from "../firebase/firebase";
 
-
 export const f = async () => {
-    const userRef = doc(testCollection, 'test1')
-    await setDoc(userRef, {
-        name: 'test1name',
-        id: 'test1id'
+  const userRef = doc(testCollection, "test1");
+  await setDoc(userRef, {
+    name: "test1name",
+    id: "test1id",
+  })
+    .then(() => {
+      console.log("Document successfully written!");
     })
-}
+    .catch((error: Error) => {
+      console.error("Error writing document: ", error);  // without error handling, there will be runtime error shown. Currently only authorized users can write to the database.
+    });
+};
--- web-client/src/translation/i18n.ts
+++ web-client/src/translation/i18n.ts
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { CONFIG } from '../config';
 
-import pl from '../locales/pl.json';
+import pl from './locales/pl.json';
 
 export default i18n
   .use(initReactI18next)
